88169c8f223acf0940b1b23e1317cad545ff0c96
tobias <tobias.omondi@student.moringaschool.com>
2023-07-13 15:37:53 +0300

    my-assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    //returns the number of blocks given a value
+    if (blocks<42){
+      return(42-blocks);
+    }
+    else {
+  
+    return(blocks-42);
+  }
+  }
+function distanceFromHqInFeet (feets) {
+  return distanceFromHqInBlocks(feets)*264;
+}
+
+function distanceTravelledInFeet(startblock,endblock){
+  if (endblock>startblock){
+    return((endblock-startblock)*264);
+  }
+  else
+    return((startblock-endblock)*264);
+}
+function calculatesFarePrice(start,destination){
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance<= 400){
+      return 0;
+    }
+    if (distance>=400 && distance<=2000){
+      return (distance-400)* 0.02;
+    }
+    else if(distance>=2000&& distance<=2500){
+      return 25;
+    }
+    else if (distance > 2500){
+      return "cannot travel that far";
+    }
+  }
